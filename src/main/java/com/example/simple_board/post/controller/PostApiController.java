e5b3fce26223f44f8fb5749a562a8e28a545eb6f
minuk07 <lmw3164@naver.com>
2025-01-23 18:56:08 +0900

    1-23_게시글 열람, 게시글 전체 확인, 게시글 삭제 api 구현

--- src/main/java/com/example/simple_board/post/controller/PostApiController.java
+++ src/main/java/com/example/simple_board/post/controller/PostApiController.java
@@ -3,13 +3,13 @@ package com.example.simple_board.post.controller;
 import com.example.simple_board.board.model.BoardRequest;
 import com.example.simple_board.post.db.PostEntity;
 import com.example.simple_board.post.model.PostRequest;
+import com.example.simple_board.post.model.PostViewRequest;
 import com.example.simple_board.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/post")
@@ -19,11 +19,34 @@ public class PostApiController {
     private final PostService postService; //controller는 service와 상호작용.
 
     @PostMapping("")
-    public PostEntity create(
+    public PostEntity create( //게시글 생성
         @Valid //postRequest의 형식에 맞는지 검증.
         @RequestBody //Json body로 요청을 받겠다는 어노테이션
         PostRequest postRequest
     ){
         return postService.create(postRequest);
+    }
+
+    @PostMapping("/view") //클릭하면 비밀번호를 입력해야 볼 수 있으므로 단순한 get으로는 불가능.
+    public PostEntity view(
+        @Valid
+        @RequestBody PostViewRequest postViewRequest
+    ){
+        return postService.view(postViewRequest);
+    }
+
+    @GetMapping("/all")
+    public List<PostEntity> list(
+
+    ){
+        return postService.all();
+    }
+
+    @PostMapping("/delete") //비밀번호를 넣어야 하기 때문에 단순 deleteMapping으로 불가능.
+    public void delete(
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ){
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/simple_board/post/service/PostService.java
+++ src/main/java/com/example/simple_board/post/service/PostService.java
@@ -3,10 +3,13 @@ package com.example.simple_board.post.service;
 import com.example.simple_board.post.db.PostEntity;
 import com.example.simple_board.post.db.PostRepository;
 import com.example.simple_board.post.model.PostRequest;
+import com.example.simple_board.post.model.PostViewRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,4 +33,47 @@ public class PostService {
         return postRepository.save(entity);
     }
 
+    //1.게시글이 있는지 확인
+    //2.비밀번호가 맞는지 확인
+    public PostEntity view(@Valid PostViewRequest postViewRequest) {
+
+        return postRepository.findById(postViewRequest.getPostId())
+                //postViewRequest 객체의 postId를 가져와 postRepository를 통해 데이터베이스에서 해당 id에 해당하는 게시글을 조회
+                .map( it -> {
+                    //해당 Entity가 존재할 때
+                    if(!it.getPassword().equals(postViewRequest.getPassword())){ //요청된 password와 조회된 게시글의 password가 동일하지 않으면
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+                    return it; //비밀번호가 일치하면 해당 Entity 리턴
+                }).orElseThrow( //Optional에 값이 비어 있는 경우(findById 결과가 비어있는 경우)
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다. :" + postViewRequest.getPostId());
+                        }
+                );
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(@Valid PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                //postViewRequest 객체의 postId를 가져와 postRepository를 통해 데이터베이스에서 해당 id에 해당하는 게시글을 조회
+                .map( it -> {
+                    //해당 Entity가 존재할 때
+                    if(!it.getPassword().equals(postViewRequest.getPassword())){ //요청된 password와 조회된 게시글의 password가 동일하지 않으면
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+                    it.setStatus("UNREGISTRED");
+                    postRepository.save(it);
+                    return it; //비밀번호가 일치하면 해당 Entity 리턴
+                }).orElseThrow( //Optional에 값이 비어 있는 경우(findById 결과가 비어있는 경우)
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다. :" + postViewRequest.getPostId());
+                        }
+                );
+
+    }
 }
